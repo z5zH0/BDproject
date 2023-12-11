369bb51bb7d8f5905603d182d0c6d2de3f8a8b46
yejin <kimyejin0044@gmail.com>
2023-12-11 20:12:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  '김예진': [201800935],
+  '박현빈': [202001599]
 })
 
 df
